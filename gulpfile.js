1f4fc2ac8c27d584c03d56e01d6a650a1e53fde8
sean-codes <sean_codes@outlook.com>
2017-12-22 00:31:17 -0600

    add watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 var gulp = require('gulp')
 var sass = require('gulp-sass')
 
+gulp.task('watch', function() {
+   gulp.watch('./src/**/*', ['css', 'js'])
+})
+
 gulp.task('css', function() {
    return gulp.src('./src/**/*.scss')
       .pipe(sass().on('error', sass.logError))
